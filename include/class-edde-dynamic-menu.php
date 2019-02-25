a7c593cc38d0fc2f9f6d61dbd4605c4ec9e1d589
alinavalovenko <alina.valovenko@gmail.com>
2019-02-25 16:51:11 +0200

    added Selections menus

--- include/class-edde-dynamic-menu.php
+++ include/class-edde-dynamic-menu.php
@@ -3,6 +3,7 @@ if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 	class EddE_Dynamic_Menu {
 
 		public function __construct() {
+			add_filter( 'register_post_type_args', array( $this, 'download_custom_post_type_args' ), 20, 2 );
 			add_action( 'init', array( $this, 'edde_register_menus' ) );
 			add_filter( 'wp_nav_menu_items', array( $this, 'edde_add_dynamic_section' ), 10, 2 );
 		}
@@ -33,13 +34,21 @@ if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 			if ( "edd-dynamic-menu" === $args->theme_location ) {
 				switch ( $user_status ) {
 					case 'customer':
-						$items .= '<li>' . 'customer' . '</li>';
+						$edd_settings     = get_option( 'edd_settings' );
+						$purchase_page_id = $edd_settings['purchase_history_page'];
+						$items            .= '<li id="edde-my-purchases"><a href="' . get_the_permalink( $purchase_page_id ) . '">' . get_the_title( $purchase_page_id ) . '</a>' . do_shortcode( "[download_history]" ) . '</li>';
+						$items            .= '<li id="edde-logout"><a href="' . wp_logout_url() . '">' . esc_html__( 'Logout' ) . '</a></li>';
+						$items            .= '<li><a href="' . get_post_type_archive_link("downloads").'">'. esc_html__('Downloads').'</a>';
+						$items            .= '<div class="col-6">' . $this->daily_selection_list_render() . '</div>';
+						$items            .= '<div class="col-6">' . $this->weekly_selection_list_render(). '</div>';
+						$items            .= '</li>';
 						break;
 					case 'guest':
 						$items .= '<li>' . 'guest' . '</li>';
+						$items .= '<li id="edde-logout"><a href="' . wp_logout_url() . '">' . esc_html__( 'Logout' ) . '</a></li>';
 						break;
 					case 'unknown':
-						$items .= '<li>' . 'User is unknown' . '</li>';
+						$items .= '<li id="edde-login"><a href="' . wp_login_url() . '">' . esc_html__( 'Login' ) . '</a> / <a href="' . wp_registration_url() . '"> ' . esc_html__( 'Registration' ) . '</a></li>';
 						break;
 				}
 			} else {
@@ -64,6 +73,79 @@ if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 
 			return $status;
 		}
+
+		function selection_menu_list_render() {
+
+		}
+
+		function weekly_selection_list_render() {
+			$args = array(
+				'type'            => 'daily',
+				'limit'           => 8,
+				'format'          => 'custom',
+				'show_post_count' => false,
+				'echo'            => false,
+				'order'           => 'DESC',
+				'post_type'       => 'download'
+			);
+
+			$links = wp_get_archives( $args );
+			preg_match_all( '/<a(.*)<\/a>/U', $links, $output_array );
+			$item  = '<span>' . esc_html__( '	Weekly Results Menu' ) . '</span>';
+			$item  .= '<ul class="selection sub-nemu">';
+			$count = count( $output_array[0] );
+			for ( $i = 1; $i < $count; $i ++ ) {
+				$item .= '<li>' . $output_array[0][ $i ] . '</li>';
+			}
+			$item .= '</ul>';
+
+			return $item;
+		}
+
+		function daily_selection_list_render() {
+			$args       = array(
+				'type'            => 'daily',
+				'limit'           => 1,
+				'format'          => 'custom',
+				'show_post_count' => false,
+				'echo'            => false,
+				'order'           => 'DESC',
+				'post_type'       => 'download'
+			);
+			$item       = '<span>' . esc_html__( 'Daily Selections Menu' ) . '</span>';
+			$item       .= '<div>' . wp_get_archives( $args ) . '</div>';
+			$today      = getdate();
+			$selections = get_posts( array(
+				'numberposts' => '-1',
+				'orderby'     => 'date',
+				'order'       => 'DESC',
+				'post_type'   => 'download',
+				'date_query'  => array(
+					array(
+						'year'  => $today['year'],
+						'month' => $today['mon'],
+						'day'   => $today['mday'],
+					),
+				),
+			) );
+			$item       .= '<ul class="selection sub-nemu">';
+			foreach ( $selections as $selection ) {
+				$item .= '<li><a href="' . get_the_permalink( $selection->ID ) . '">' . $selection->post_title . '</a></li>';
+			}
+			$item .= '</ul>';
+
+			return $item;
+		}
+
+		function download_custom_post_type_args( $args, $post_type ) {
+			if ( "download" === $post_type ) {
+				$args['has_archive'] = true;
+				$args['public']      = true;
+			}
+
+			return $args;
+		}
+
 	}
 }
 new EddE_Dynamic_Menu();
