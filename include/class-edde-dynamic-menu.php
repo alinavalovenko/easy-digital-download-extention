3560d62334ed9e77a110e61c846bdc795091f258
alinavalovenko <alina.valovenko@gmail.com>
2019-02-28 14:13:41 +0200

    menu updated

--- include/class-edde-dynamic-menu.php
+++ include/class-edde-dynamic-menu.php
@@ -2,7 +2,9 @@
 if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 	class EddE_Dynamic_Menu {
 
+		private $meta_key;
 		public function __construct() {
+			$this->meta_key='edd_download_files';
 			add_filter( 'register_post_type_args', array( $this, 'download_custom_post_type_args' ), 20, 2 );
 			add_action( 'init', array( $this, 'edde_register_menus' ) );
 			add_filter( 'wp_nav_menu_items', array( $this, 'edde_add_dynamic_section' ), 10, 2 );
@@ -37,17 +39,12 @@ if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 						$edd_settings     = get_option( 'edd_settings' );
 						$purchase_page_id = $edd_settings['purchase_history_page'];
 						$items            .= '<li><a href="' . get_post_type_archive_link("downloads").'">'. esc_html__('Downloads').'</a>';
-						$items            .= '<div class="col-6">' . $this->daily_selection_list_render() . '</div>';
-						$items            .= '<div class="col-6">' . $this->weekly_selection_list_render(). '</div>';
+						$items            .= '<div class="col-6">' . $this->selection_list_render() . '</div>';
 						$items            .= '</li>';
 						$items            .= '<li id="edde-my-purchases"><a href="' . get_the_permalink( $purchase_page_id ) . '">' . get_the_title( $purchase_page_id ) . '</a>' . do_shortcode( "[download_history]" ) . '</li>';
 						$items            .= '<li id="edde-logout"><a href="' . wp_logout_url() . '">' . esc_html__( 'Logout' ) . '</a></li>';
 						break;
 					case 'guest':
-						$items            .= '<li><a href="' . get_post_type_archive_link("downloads").'">'. esc_html__('Downloads').'</a>';
-						$items            .= '<div class="col-6">' . $this->daily_selection_list_render() . '</div>';
-						$items            .= '<div class="col-6">' . $this->weekly_selection_list_render(). '</div>';
-						$items            .= '</li>';
 						$items .= '<li id="edde-logout"><a href="' . wp_logout_url() . '">' . esc_html__( 'Logout' ) . '</a></li>';
 						break;
 					case 'unknown':
@@ -69,12 +66,27 @@ if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 		function get_user_status() {
 			$status  = 'unknown';
 			$user_id = get_current_user_id();
+			$args = array(
+				'numberposts'      => 1,
+				'orderby'          => 'date',
+				'order'            => 'DESC',
+				'post_type'        => 'download',
+				'post_status'      => 'publish',
+			);
+			$last_selection = get_posts( $args, OBJECT  );
 			if ( $user_id ) {
 				$customer = new EDD_Customer( $user_id, true );
-				if ( $customer->purchase_count > 0 ) {
-					$status = 'customer';
-				} else {
-					$status = 'guest';
+				$payments =$customer->get_payments();
+				$customer_payments = end($payments);
+				$customer_downloads = $customer_payments->downloads;
+				$last_download = (string)$last_selection[0]->ID;
+
+				foreach ($customer_downloads as $item){
+					if($last_download ==$item['id']){
+						$status = 'customer';
+					} else {
+						$status = 'guest';
+					}
 				}
 			}
 
@@ -115,35 +127,21 @@ if ( ! class_exists( 'EddE_Dynamic_Menu' ) ) {
 		 *
 		 * @return string
 		 */
-		function daily_selection_list_render() {
-			$args       = array(
-				'type'            => 'daily',
-				'limit'           => 1,
-				'format'          => 'custom',
-				'show_post_count' => false,
-				'echo'            => false,
-				'order'           => 'DESC',
-				'post_type'       => 'download'
+		function selection_list_render() {
+			$args = array(
+				'numberposts'      => 1,
+				'orderby'          => 'date',
+				'order'            => 'DESC',
+				'post_type'        => 'download',
+				'post_status'      => 'publish',
 			);
+			$last_selection = get_posts( $args, OBJECT  )[0];
+			$downloads =get_post_meta($last_selection->ID, $this->meta_key, true);
 			$item       = '<span>' . esc_html__( 'Daily Selections Menu' ) . '</span>';
-			$item       .= '<div>' . wp_get_archives( $args ) . '</div>';
-			$today      = getdate();
-			$selections = get_posts( array(
-				'numberposts' => '-1',
-				'orderby'     => 'date',
-				'order'       => 'DESC',
-				'post_type'   => 'download',
-				'date_query'  => array(
-					array(
-						'year'  => $today['year'],
-						'month' => $today['mon'],
-						'day'   => $today['mday'],
-					),
-				),
-			) );
+			$item       .= '<div>' .$last_selection->post_title . '</div>';
 			$item       .= '<ul class="selection sub-nemu">';
-			foreach ( $selections as $selection ) {
-				$item .= '<li><a href="' . get_the_permalink( $selection->ID ) . '">' . $selection->post_title . '</a></li>';
+			foreach ( $downloads as $d_item ) {
+				$item .= '<li><a href="' . $d_item['file'] . '">' . $d_item['name'] . '</a></li>';
 			}
 			$item .= '</ul>';
 
